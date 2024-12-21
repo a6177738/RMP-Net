66382e0511916a38253e168c07dd94145b45c3c8
LiXiaofan <55318914+a6177738@users.noreply.github.com>
2024-12-21 13:48:27 +0800

    Update model_img.py

--- model_img.py
+++ model_img.py
@@ -8,10 +8,10 @@ import torch.nn.functional as F
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, mode = 'train'):
         super(Net, self).__init__()
 
-        self.prior = tm.resnet18(pretrained=True)
+        self.prior = tm.resnet18(pretrained=False)
 
         self.fc = nn.Linear(1000, 256)
         self.fc1 = nn.Linear(256, 1)
@@ -19,14 +19,11 @@ class Net(nn.Module):
 
     def forward(self, img):
         x = self.prior(img)
-
         feature = self.fc(x)
-        feature = torch.nn.functional.normalize(feature, dim=1)
+        feature = torch.nn.functional.normalize(feature, dim=1).squeeze(dim=1)
 
-        mu = torch.sigmoid(self.fc1(feature))
-        theta = F.softplus(self.fc2(feature))
+        mu = torch.sigmoid(self.fc1(feature)).squeeze(dim=1)
+        theta = F.softplus(self.fc2(feature)).squeeze(dim=1)
 
         return  feature, mu, theta
-        #return mu
-
 
